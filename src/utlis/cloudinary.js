e35f550d6f6c28eb1e09ad7d33df0ab6ab6b1aa3
sobiboby1 <sobiboby65@gmail.com>
2024-10-19 10:15:42 +0500

    setting up cloudinary

---
+++ src/utlis/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: 'sobiboby',
+    api_key: "822612624936922",
+    api_secret: "sIc2UdvC8gusF5eri6hMfKNT54E"
+});
+
